a3776fcc8de2fe899ffd852623c73639c043e7be
sandeep <sandeepsekhar555@gmail.com>
2018-12-01 00:29:13 +0800

    added selenium jars

--- src/Tests/SecondTest.java
+++ src/Tests/SecondTest.java
@@ -2,11 +2,16 @@ package Tests;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class SecondTest {
+	WebDriver _driver = new ChromeDriver();
+	
   @Test
   public void f() {
+	   
   }
   @BeforeTest
   public void beforeTest() {
